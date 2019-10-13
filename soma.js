2ede420c3be111b63a949368be44e12f83edd4ae
Dmitry Nets <dmitrynets@gmail.com>
2019-10-14 01:06:54 +0300

    improve project dates

--- soma.js
+++ soma.js
@@ -82,12 +82,15 @@ class ProjectorDate {
     if (
       (positions[0] > 12 || positions[1] > 30 || positions[2] > 21)
       ||
-      (positions[0] <= 0 || positions[1] <= 0 || positions[2] <= 0)) {
+      (positions[0] < 0 || positions[1] < 0 || positions[2] < 0)) {
       return { error: true };
     }
+    console.log(positions);
     this.dates = [];
-    var next_date = new Date();
-    var soma_projected = new CalendarSoma(next_date);
+    this.next_date = new Date();
+    this.soma_projected = new CalendarSoma(this.next_date);
+    this.dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+
     for (var i = cycles; i > 0; i--) {
       this.tries = 0;
       do {
@@ -95,11 +98,34 @@ class ProjectorDate {
         if (this.tries > 840) {
           return { error: true };
         }
-        next_date.setDate(next_date.getDate() + 1);
-        soma_projected = new CalendarSoma(next_date);
+        this.next_date.setDate(this.next_date.getDate() + 1);
+        this.soma_projected = new CalendarSoma(this.next_date);
+        // Check wildcards
+        if (positions[0] == 0) {
+          this.target_soma = this.soma_projected.soma;
+        } else {
+          this.target_soma = positions[0];
+        }
+        if (positions[1] == 0) {
+          this.target_indra = this.soma_projected.indra;
+        } else {
+          this.target_indra = positions[1];
+        }
+        if (positions[2] == 0) {
+          this.target_agni = this.soma_projected.agni;
+        } else {
+          this.target_agni = positions[2];
+        }
+        // console.log(`Target soma: ${this.target_soma}`);
+        // console.log(`Target indra: ${this.target_indra}`);
+        // console.log(`Target agni: ${this.target_agni}`);
+        // console.log(`Current soma: ${this.soma_projected.soma}`);
+        // console.log(`Current indra: ${this.soma_projected.indra}`);
+        // console.log(`Current agni: ${this.soma_projected.agni}`);
       }
-      while (soma_projected.toString() != `${positions[0]}.${positions[1]}.${positions[2]}`);
-      this.dates.push(next_date.toDateString());
+      while (
+        (this.target_soma != this.soma_projected.soma) || (this.target_indra != this.soma_projected.indra) || (this.target_agni != this.soma_projected.agni));
+      this.dates.push({ positions: `${this.soma_projected.soma}.${this.soma_projected.indra}.${this.soma_projected.agni}`, date: this.next_date.toLocaleDateString('ru-RU') });
     }
   }
   get dateList() {
@@ -108,15 +134,17 @@ class ProjectorDate {
 }
 
 jQuery.datetimepicker.setLocale('ru');
+const stringToDate = function (dateString) {
+  const [dd, mm, yyyy] = dateString.split(".");
+  return new Date(`${yyyy}-${mm}-${dd}`);
+};
 
 $(function () {
   var calendar = new CalendarView;
   $('#datetimepicker').datetimepicker({
-    format: 'd.m.YH:i',
+    format: 'd.m.Y',
     inline: true,
     lang: 'ru',
-    // theme: 'dark',
-    // style: 'position: fixed; top: 5px; right: 5px; box-shadow: none;',
     timepicker: false,
     onChangeDateTime: function (ct, $i) {
       var d = $('#datetimepicker').datetimepicker('getValue');
@@ -131,6 +159,7 @@ $(function () {
       calendar = new CalendarView(d);
     }
   });
+
   $("button.calendar").toggle(function () {
     $("#datetimepicker").datetimepicker("show");
   }, function () {
@@ -143,14 +172,40 @@ $(function () {
 
   $('#projector_submit').on('click', function () {
     $('#projector_results').html('');
-    projector = new ProjectorDate([$('#projector_soma').val(), $('#projector_indra').val(), $('#projector_agni').val()], $('#projector_cycles').val());
+    projector = new ProjectorDate([parseInt($('#projector_soma').val()), parseInt($('#projector_indra').val()), parseInt($('#projector_agni').val())], parseInt($('#projector_cycles').val()));
     $('#projector_results').append('<hr>');
     if (projector.error) {
       $('#projector_results').append('<li>Позиции заданы неверно!</li>');
     } else {
       projector.dateList.forEach(element => {
-        $('#projector_results').append('<div class="col">' + element + '</div>');
+        $('#projector_results').append('<button class="btn btn-light mx-3 mb-3 projected_link"><div><strong>' + element.positions + '</strong></div>' + '<span class="projected_date">' + element.date + '</span></div></button>');
       });
     }
+  });
+
+  $('body').on('click', 'button.projected_link', function () {
+    var d = $(this).find('.projected_date').html();
+    soma = new CalendarView(new Date(stringToDate(d)));
+    // $('#datetimepicker').datetimepicker("setDate", stringToDate(d));
+    $('#datetimepicker').datetimepicker({
+      format: 'd.m.Y',
+      inline: true,
+      lang: 'ru',
+      value: stringToDate(d),
+      timepicker: false,
+      onChangeDateTime: function (ct, $i) {
+        var d = $('#datetimepicker').datetimepicker('getValue');
+        calendar = new CalendarView(d);
+      },
+      onChangeMonth: function (ct, $i) {
+        var d = $('#datetimepicker').datetimepicker('getValue');
+        calendar = new CalendarView(d);
+      },
+      onChangeYear: function (ct, $i) {
+        var d = $('#datetimepicker').datetimepicker('getValue');
+        calendar = new CalendarView(d);
+      }
+    });
+    $('#projector_modal').modal('hide');
   });
 });
